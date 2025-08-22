734c46dc5c6fb036773ecee4dc360908b8b52156
Percevale <denis_shagun@mail.ru>
2025-08-22 17:17:06 +0300

    delete config files

--- server/src/index.ts
+++ server/src/index.ts
@@ -9,7 +9,7 @@ import errorHandler from './middlewares/errorHandler';
 import { sequelize } from './models/db';
 
 const app: Express = express();
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT;
 
 const allowedOrigins = ['http://localhost:5173', 'http://client:5173'];
 
